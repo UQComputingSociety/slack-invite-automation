f90fe07686f9231e72a7b2e027ca8f89bce8efc9
lewisjb <lbobbermen@bigpond.com>
2016-07-27 20:19:18 +1000

    Added blacklist

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,8 @@ var router = express.Router();
 var request = require('request');
 var config = require('../config');
 
+var blacklist = ['mailinator.com'];
+
 router.get('/', function(req, res) {
   res.render('index', { community: config.community, slackUrl: config.slackUrl,
                         tokenRequired: !!config.inviteToken });
@@ -10,47 +12,59 @@ router.get('/', function(req, res) {
 
 router.post('/invite', function(req, res) {
   if (req.body.email && (!config.inviteToken || (!!config.inviteToken && req.body.token === config.inviteToken))) {
-    request.post({
-        url: 'https://'+ config.slackUrl + '/api/users.admin.invite',
-        form: {
-          email: req.body.email,
-          token: config.slacktoken,
-          set_active: true
-        }
-      }, function(err, httpResponse, body) {
-        // body looks like:
-        //   {"ok":true}
-        //       or
-        //   {"ok":false,"error":"already_invited"}
-        if (err) { return res.send('Error:' + err); }
-        body = JSON.parse(body);
-        if (body.ok) {
-          res.render('result', {
-            community: config.community,
-            message: 'Success! Check "'+ req.body.email +'" for an invite from Slack.'
-          });
-        } else {
-          var error = body.error;
-          if (error === 'already_invited' || error === 'already_in_team') {
+    var invalid = false;
+    for(var i = 0; i < blacklist.length; i++) {
+      if(req.body.email.split('@')[1] === blacklist[i]) { invalid = true; break; }
+    }
+    if(invalid) {
+      res.render('result', {
+        community: config.community,
+        message: 'Failed! Unsupported email',
+        isFailed: true
+      });
+    }else { 
+      request.post({
+          url: 'https://'+ config.slackUrl + '/api/users.admin.invite',
+          form: {
+            email: req.body.email,
+            token: config.slacktoken,
+            set_active: true
+          }
+        }, function(err, httpResponse, body) {
+          // body looks like:
+          //   {"ok":true}
+          //       or
+          //   {"ok":false,"error":"already_invited"}
+          if (err) { return res.send('Error:' + err); }
+          body = JSON.parse(body);
+          if (body.ok) {
             res.render('result', {
               community: config.community,
-              message: 'Success! You were already invited.<br>' +
-                       'Visit <a href="https://'+ config.slackUrl +'">'+ config.community +'</a>'
+              message: 'Success! Check "'+ req.body.email +'" for an invite from Slack.'
             });
-            return;
-          } else if (error === 'invalid_email') {
-            error = 'The email you entered is an invalid email.'
-          } else if (error === 'invalid_auth') {
-            error = 'Something has gone wrong. Please contact a system administrator.'
-          }
+          } else {
+            var error = body.error;
+            if (error === 'already_invited' || error === 'already_in_team') {
+              res.render('result', {
+                community: config.community,
+                message: 'Success! You were already invited.<br>' +
+                         'Visit <a href="https://'+ config.slackUrl +'">'+ config.community +'</a>'
+              });
+              return;
+            } else if (error === 'invalid_email') {
+              error = 'The email you entered is an invalid email.'
+            } else if (error === 'invalid_auth') {
+              error = 'Something has gone wrong. Please contact a system administrator.'
+            }
 
-          res.render('result', {
-            community: config.community,
-            message: 'Failed! ' + error,
-            isFailed: true
-          });
-        }
-      });
+            res.render('result', {
+              community: config.community,
+              message: 'Failed! ' + error,
+              isFailed: true
+            });
+          }
+        });
+    }
   } else {
     var errMsg = [];
     if (!req.body.email) {
